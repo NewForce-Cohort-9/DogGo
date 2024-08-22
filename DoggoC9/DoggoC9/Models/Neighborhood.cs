178713bd9cebe147345a1b107e016d7afc96faa6
Tommy-Spurlock <tspurloc@gmail.com>
2024-08-22 09:11:35 -0400

    DogGo pt 1

---
+++ DoggoC9/DoggoC9/Models/Neighborhood.cs
@@ -0,0 +1,8 @@
+﻿namespace DoggoC9.Models
+{
+    public class Neighborhood
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DoggoC9/DoggoC9/Repositories/IWalkerRepository.cs
@@ -0,0 +1,13 @@
+﻿using DoggoC9.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+
+namespace DoggoC9.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+    }
+}
