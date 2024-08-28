ec1a3edd69905096e18324aadd132cbc2a890735
Tommy-Spurlock <tspurloc@gmail.com>
2024-08-28 09:43:09 -0400

    done with book 2

--- DoggoC9/DoggoC9/Models/Owner.cs
+++ DoggoC9/DoggoC9/Models/Owner.cs
@@ -1,12 +1,26 @@
-﻿namespace DoggoC9.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoggoC9.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+     
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+        [Required]
+        [StringLength(55, MinimumLength = 5)]
         public string Address { get; set; }
+        [Phone]
+
         public string Phone { get; set; }
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
         public Neighborhood neighborhood { get; set; }
 
--- DoggoC9/DoggoC9/obj/Debug/net6.0/DoggoC9.AssemblyInfo.cs
+++ DoggoC9/DoggoC9/obj/Debug/net6.0/DoggoC9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoggoC9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717c0cb61d8db8827f1af4e720a0dcb74b109828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718b5d37429bde08d200aa82a6ee44034c7fdcba")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoggoC9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoggoC9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
